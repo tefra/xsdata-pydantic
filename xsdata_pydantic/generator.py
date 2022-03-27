4b3989cb9702731f917ca293bdb09658d5f04dac
Christodoulos Tsoulloftas <chris@komposta.net>
2022-03-27 22:10:07 +0300

    Register new types in the converter

--- xsdata_pydantic/generator.py
+++ xsdata_pydantic/generator.py
@@ -3,6 +3,9 @@ from typing import Dict
 from xsdata.formats.dataclass.filters import Filters
 from xsdata.formats.dataclass.generator import DataclassGenerator
 from xsdata.models.config import GeneratorConfig
+from xsdata.utils.text import stop_words
+
+stop_words.update(("schema", "validate"))
 
 
 class PydanticGenerator(DataclassGenerator):
@@ -17,10 +20,19 @@ class PydanticFilters(Filters):
     @classmethod
     def build_import_patterns(cls) -> Dict[str, Dict]:
         patterns = super().build_import_patterns()
+        type_patterns = cls.build_type_patterns
         patterns.update(
             {
                 "dataclasses": {"field": [" = field("]},
                 "pydantic.dataclasses": {"dataclass": ["@dataclass"]},
+                "xsdata.models.datatype": {},
+                "xsdata_pydantic.datatype": {
+                    "XmlDate": type_patterns("XmlDate"),
+                    "XmlDateTime": type_patterns("XmlDateTime"),
+                    "XmlDuration": type_patterns("XmlDuration"),
+                    "XmlPeriod": type_patterns("XmlPeriod"),
+                    "XmlTime": type_patterns("XmlTime"),
+                },
             }
         )
 
