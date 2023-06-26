ea259208fff921a0b1c670e31e8fa18b259d9899
Talley Lambert <talley.lambert@gmail.com>
2023-06-26 16:53:47 -0400

    fix compat, add ugly test

--- tests/fixtures/common.py
+++ tests/fixtures/common.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from typing import List, Optional
 
 from pydantic.dataclasses import dataclass
+from pydantic import BaseModel
+from pydantic import Field
 
 
 
@@ -22,3 +24,16 @@ class TypeB(TypeA):
 class TypeC(TypeB):
     four: List[datetime] = field(default_factory=list, metadata={"format": "%d %B %Y %H:%M"})
     any: Optional[object] = field(default=None, metadata={"type": "Wildcard"})
+
+
+class BTypeA(BaseModel):
+    one: str
+    two: float
+    
+class BTypeB(BTypeA):
+    one: str
+    three: bool = Field(default=True)
+
+class BTypeC(BTypeB):
+    four: List[datetime] = Field(default_factory=list, metadata={"format": "%d %B %Y %H:%M"})
+    any: Optional[object] = Field(default=None, metadata={"type": "Wildcard"})
